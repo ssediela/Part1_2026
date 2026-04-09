8b866fa5fc44718054047c685369c016d002e5ee
ssediela <131776835+ssediela@users.noreply.github.com>
2026-04-09 15:17:37 +0200

    UnitTestCreated

---
+++ StringManupulationWitsClasses/src/test/java/com/mycompany/stringmanupulationwitsclasses/ArcAccountTest.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.stringmanupulationwitsclasses;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author sedie
+ */
+public class ArcAccountTest {
+    
+    public ArcAccountTest() {
+    }
+
+    @Test
+    public void testSetEmail() {
+    }
+
+    @Test
+    public void testSetPin() {
+    }
+
+    @Test
+    public void testGetEmail() {
+    }
+
+    @Test
+    public void testGetPin() {
+    }
+
+    @Test
+    public void testVerfyEmail() {
+    }
+
+    @Test
+    public void testVerifyPin() {
+    }
+    
+}
